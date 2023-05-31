2c6965fe70f15f3183813498592be787efc6920a
Tony <tnp087@gmail.com>
2023-05-30 20:38:20 -0400

    form formatted + newrecipes route and controller

--- food-dude/controllers/newrecipes.js
+++ food-dude/controllers/newrecipes.js
@@ -6,6 +6,17 @@ const index = async (req,res) => {
   })
 }
 
+const create = async (req,res) => {
+  const newRecipes = await newRecipe.findById(req.params.id)
+  newRecipe..push(req.body); 
+  try {
+    await flight.save();
+  } catch (err) {
+    console.log(err)
+  }
+  res.redirect(`/flights/${flight._id}`)
+}
+
 module.exports = {
   index, 
 }
--- food-dude/routes/newrecipes.js
+++ food-dude/routes/newrecipes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const newRecipesCtrl = require('../controllers/newrecipes')
+const newRecipesCtrl = require('../controllers/newrecipes');
+const newrecipe = require('../models/newrecipe');
 
 router.get('/', newRecipesCtrl.index)
-
+router.post('/posts/:id', newRecipesCtrl.create)
 module.exports = router;
